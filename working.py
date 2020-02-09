6a126091d48be96143dc0575988d7021e9889541
Liam Halpin <2383998H@student.gla.ac.uk>
2020-02-09 19:20:46 +0000

    added function that prints a sudoku grid

---
+++ working.py
@@ -0,0 +1,26 @@
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+
+# function that prints a sudoku grid
+def print_board(board):
+    for i in range(len(board)):
+        if (i % 3 == 0) and (i != 0):
+            print("------------------------")
+        for j in range(len(board[0])):
+            if (j % 3 == 0) and (j != 0):
+                print(" | ", end="")
+            
+            if j == 8:
+                print(str(board[i][j]))
+            else:
+                print(str(board[i][j]) + " ", end="")

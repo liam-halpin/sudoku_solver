a1c5555804b9491e50dcdcb3f40c940f209f9bb9
Liam Halpin <2383998H@student.gla.ac.uk>
2020-02-10 16:46:46 +0000

    added recursive function to solve board

--- main.py
+++ main.py
@@ -10,6 +10,43 @@ board = [
     [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ]
 
+# Recursive function to solve the grid
+def solve(board):
+    find = find_empty_square(board)
+    if find:
+        row, col = find
+    else:
+        return True
+
+    for i in range(1, 10):
+        if is_board_valid(board, (row, col), i):
+            board[row][col] = i
+            if solve(board):
+                return True
+            board[row][col] = 0
+    return False
+
+
+def is_board_valid(board, pos, num):
+    # Check column (y)
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+            
+    # Check row (x)
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check box (3x3)
+    box_x, box_y = pos[1]//3, pos[0]//3
+
+    for i in range(box_y*3, box_y*3+3):
+        for j in range(box_x*3, box_x*3+3):
+            if board[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
 
 # displays a sudoku grid
 def print_board(board):
@@ -25,27 +62,6 @@ def print_board(board):
             else:
                 print(str(board[i][j]) + " ", end="")
 
-def is_board_valid(board, pos, num):
-    # check column
-    for i in range(len(board)):
-        if (board[i][pos[1]] == num) and (pos[1] = != i):
-            return False
-
-    # check row
-    for i in range(len(board[0])):
-        if (board[pos[0]][i] == num) and (pos[i] != i):
-            return False
-    
-    # check box (3x3)
-    box_x, box_y = pos[1]//3, pos[0]//3
-
-    for i in range(box_y*3, box_y*3+3):
-        for j in range(box_x*3, box_x*3+3):
-            if ((i, j) != pos) and (board([i][j]) == num):
-                return False
-    
-    return True
-
 
 
 # finds an empty square on the grid
@@ -56,3 +72,8 @@ def find_empty_square(board):
                 # row, column
                 return (i, j)
     return None
+
+print_board(board)
+solve(board)
+print("\n")
+print_board(board)
